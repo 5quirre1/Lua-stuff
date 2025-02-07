db0183141db7e5d6113b82e0a6e08dbcd044cda5
Squirrel <squirrelhomebrew@gmail.com>
2025-02-07 11:08:43 -0800

    Create g.lua

---
+++ LUA/g.lua
@@ -0,0 +1,12 @@
+io.write("Enter your name: ")
+name = io.read()
+print("haiii!!!, " .. name)
+io.write("hru?? ")
+how = io.read()
+
+
+if string.find(how, "good") then
+    print("that's good lmfao")
+else
+    print("ok")
+end
